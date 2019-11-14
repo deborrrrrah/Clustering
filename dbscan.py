54cfe83b35890462e95f995f9fd79fe113d7daac
Muhammad Alif Arifin <m.alif.arifin@gmail.com>
2019-11-14 16:49:44 +0700

    add predict in dbscan

--- dbscan.py
+++ dbscan.py
@@ -1,12 +1,15 @@
 import math
 import pandas as pd
+import operator
 
 class DBSCAN :
-    self.labels = [] # -1 for noise
-    self.__X = []
-    self.__unvisited = set()
-    self.__neighbors = []
-    self.__curr_cluster = -1
+    labels = [] # -1 for noise
+    labels_predict = []
+    __X = []
+    __unvisited = set()
+    __neighbors = []
+    __neighbors_predict = []
+    __curr_cluster = -1
 
     def __init__(self, eps, min_pts) :
         self.__eps = eps
@@ -32,10 +35,18 @@ class DBSCAN :
                 if i == j :
                     pass
                 else :
-                    distance = (__eucledian(self.__X.loc[i], self.__X.loc[j]))
+                    distance = self.__eucledian(self.__X.loc[i], self.__X.loc[j])
                     if distance <= self.__eps :
                         self.__neighbors[i].append(j)
-                        self.__neighbors[j].append(i)  
+                        self.__neighbors[j].append(i)
+
+    def __init_neighbors_predict(self) :
+    # find list of neighbors' indices for predict
+        for i in range (0, len(self.__X_predict.index)) :
+            for j in range (0, len(self.__X.index)) :
+                distance = self.__eucledian(self.__X_predict.loc[i], self.__X.loc[j])
+                if distance <= self.__eps :
+                    self.__neighbors_predict[i].append(j)
 
     def __expand_cluster(self, p, pts) :
     # expand cluster until no point added
@@ -48,8 +59,8 @@ class DBSCAN :
                 nei_pts = self.__neighbors[pt]
                 if len(nei_pts) >= self.__min_pts :
                     self.__expand_cluster(pt, nei_pts)
-            if labels[pt] == -1 :
-                labels[pt] = self.__curr_cluster
+            if self.labels[pt] == -1 :
+                self.labels[pt] = self.__curr_cluster
 
     # end of helper function
 
@@ -67,7 +78,26 @@ class DBSCAN :
 
         while self.__unvisited :
             curr = self.__unvisited.pop()
-            nei_pts = self.__neighbors[i]
+            nei_pts = self.__neighbors[curr]
             if len(nei_pts) >= self.__min_pts :
                 self.__curr_cluster += 1
-                self.__expand_cluster(i, nei_pts)
+                self.__expand_cluster(curr, nei_pts)
+
+    def predict(self, X) :
+    # input : X : dataset {pandas.DataFrame}
+        self.__X_predict = X
+
+        len_X = len(self.__X_predict)
+        self.__labels_predict = [-1] * len_X
+        self.__neighbors_predict = [[] for i in range(len_X)]
+
+        self.__init_neighbors_predict()
+
+        for i in range (0, len(self.__X_predict)) :
+            keys = [i for i in range(-1, self.__curr_cluster + 1)]
+            count_labels = dict.fromkeys(keys, 0)
+            for neighbor in self.__neighbors_predict[i] :
+                count_labels[self.__labels[neighbor]] += 1
+            self.__labels_predict[i] = max(count_labels.items(), key = operator.itemgetter(1))[0]
+
+        return self.__labels_predict
